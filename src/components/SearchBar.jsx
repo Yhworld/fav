8e87dbf365d68f5e5b50baf8fe7e2fa713d6b513
Yahya <yahyahussein670@gmail.com>
2025-04-06 09:37:12 +0300

    create: search bar layout

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+
+
+function SearchBar() {
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        className="p-2 w-full border rounded-md"
+        placeholder="Search for items..."
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
